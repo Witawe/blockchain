3658d260c5197fa81286a1f425fd184233703483
Alexey <47403706+Witawe@users.noreply.github.com>
2021-03-15 15:04:26 +0900

    added 2 new function

--- as.sol
+++ as.sol
@@ -1,6 +1,7 @@
 contract Test
 {
     enum RequestType{NewHome, EditHome}
+    //enum Position{}
     
     struct Ownership
     {
@@ -37,11 +38,13 @@ contract Test
         string phoneNumber;
     }
     
-    mapping(address => Employee) private employees;
+    mapping(string => Employee) private employees;
     mapping(address => Owner) private owners;
     mapping(address => Request) private requests;
     mapping(string => Home) private homes;
     mapping(string => Ownership[]) private ownerships;
+    
+    Employee e;
     
     function AddHome(string memory _adr, uint _area, uint _cost) public {
         Home memory h;
@@ -50,7 +53,25 @@ contract Test
         h.cost = _cost;
         homes[_adr] = h;
     }
+    
     function GetHome(string memory adr) public returns (uint _area, uint _cost){
         return (homes[adr].area, homes[adr].cost);
+    }
+    
+    function AddEmployee(string memory _name, string memory _position, string memory _phoneNumber) public {
+        e.nameEmployee = _name;
+        e.position = _position;
+        e.phoneNumber = _phoneNumber;
+        employees[_name] = e;
+    }
+    
+    function GetEmployee(string memory nameEmployee) public returns (string memory _position, string memory _phoneNumber){
+        return (employees[nameEmployee].position, employees[nameEmployee].phoneNumber);
+    }
+    
+    function EditEmployee(string memory nameEmployee, string memory _newname, string memory _newposition, string memory _newphoneNumber) public {
+        employees[nameEmployee].nameEmployee = _newname;
+        employees[_newname].position = _newposition;
+        employees[_newname].phoneNumber = _newphoneNumber;
     }
 }
